7517399c61bcec1a545f42522774db7d6fb390d2
Vladimir <vladimir_schmidt@hotmail.com>
2017-10-31 10:53:49 +0100

    https redirected to http

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto']=== 'http'){
+    next();
+  }else{
+    res.redirect('http://'+ req.hostname +req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
